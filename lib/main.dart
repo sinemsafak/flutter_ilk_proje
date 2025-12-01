a9e2284a039b999fdc9d353790266134c03e2ff3
Sinem Şafak <safaksm14@gmail.com>
2025-12-01 12:37:31 +0300

    FloatingActionButton ve Material Design Kuralları

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          primary: Colors.teal,
+          secondary: Colors.purple,
+        ),
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text("başlık"),
           backgroundColor: Colors.amber,
         ),
-        body: Text("Merhaba Dünya"),
+        body: Text("Merhaba Dünya", style: TextStyle(color: Colors.red, fontSize: 24, fontWeight: FontWeight.bold),),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {  //bu bir call back fonksiyondur
+            debugPrint("Butona tıklandı");
+          },
+          child: Icon(Icons.account_circle_rounded, color: Colors.blue[100]),
+          backgroundColor: Colors.red,
+        ),
       ),
     );
   }
-
 }
