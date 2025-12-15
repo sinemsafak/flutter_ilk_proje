db5530c78598c91101ac52312a845592045af796
Sinem Şafak <safaksm14@gmail.com>
2025-12-15 14:38:18 +0300

    Container Box Decoration, Border, Image ve Shadow

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final String _img1 =
+      "https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.light(
-          primary: Colors.teal,
-          secondary: Colors.purple,
-        ),
-      ),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Başlık"),
-        ),
-      body: Center(
-        widthFactor: 2,
-        heightFactor: 2,
-        child:Container(
-          width: 100,
-          height: 100,
-          color:Colors.red,
-          child:Text("sinem"),
-        ),
-      ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {  
-            debugPrint("Tıklandı");
-          },
-          child: Icon(
-            Icons.account_circle_rounded, 
-            color: Colors.blue,
+        appBar: AppBar(title: Text("Başlık")),
+        body: Center(
+          child: Container(
+            width: 250,
+            height: 250,
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              border: Border.all(width: 4, color: Colors.purple),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(16),
+                topRight: Radius.circular(30),
+              ),
+              image: DecorationImage(
+                image: NetworkImage(_img1),
+                fit: BoxFit.scaleDown,
+                repeat: ImageRepeat.repeat,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.green,
+                    blurRadius: 20,
+                    offset: Offset(4, 4),
+                  ),
+                  BoxShadow(
+                    color: Colors.yellow,
+                    blurRadius: 10,
+                    offset: Offset(0, -4),
+                  )
+                ]
             ),
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                "sinem",
+                style: TextStyle(
+                  fontSize: 50,
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 6,
+                      color: Colors.black,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
