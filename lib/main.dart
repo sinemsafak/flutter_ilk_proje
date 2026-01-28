83f9a9e74dc536fbc1cf8dacf0ffd340357237ed
Sinem Şafak <safaksm14@gmail.com>
2026-01-28 19:18:09 +0300

    main page güncellemesi

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ilk_proje/alert_dialog_page.dart';
-// import 'pages/snackbar_page.dart';
-// import 'pages/bottom_sheet_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: AlertDialogPage(), // 👈 SADECE BURAYI DEĞİŞTİR
+      home: AlertDialogPage(), 
     );
   }
 }
